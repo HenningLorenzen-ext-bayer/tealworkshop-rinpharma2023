00d859c3c186eb9f04065385c5240f871b4e48cd
Henning Lorenzen <henning.lorenzen.ext@bayer.com>
2024-01-17 16:50:01 +0000

    add (some) answers to non-CDISC data examples

--- code/02-noncdiscdata.R
+++ code/02-noncdiscdata.R
@@ -14,9 +14,37 @@
 library(teal.modules.general)
 
 app <- init(
-  data = ,
-  modules = ,
-  header = "R/Pharma 2023 teal Workshop"
+  data = teal_data(
+    dataset(dataname = "iris", x = iris),
+    dataset(dataname = "mtcars", x = mtcars)
+  ),
+  modules = tm_data_table(label = "Data table mod"),
+  header = "R/Pharma 2023 teal Workshop",
+  filter = teal_slices(
+    teal_slice(dataname = "iris", varname = "Sepal.Length"),
+    teal_slice(dataname = "iris", varname = "Species"),
+    teal_slice(
+      dataname = "mtcars",
+      varname = "cyl",
+      selected = c(6, 8)
+    ),
+    teal_slice(
+      dataname = "mtcars",
+      varname = "gear",
+      anchored = TRUE
+    ),
+    teal_slice(
+      dataname = "mtcars",
+      varname = "am",
+      anchored = TRUE,
+      selected = "1"
+    ),
+    teal_slice("iris",
+               expr = "Petal.Length > 4 & Petal.Width > 1",
+               id = "customiris",
+               title = "My custom filter"),
+    count_type = "all"
+  )
 )
 
 shinyApp(app$ui, app$server)
